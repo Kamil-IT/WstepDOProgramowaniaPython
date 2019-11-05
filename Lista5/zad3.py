d680053a387f1ad740851461e84d71399ec6c360
kkwolny <kkwolny@vp.pl>
2019-11-05 11:22:14 +0100

    Edit list 3

--- Lista5/zad3.py
+++ Lista5/zad3.py
@@ -5,8 +5,12 @@ class Liczba_losowa:
     def losuj01(self):
         return random.random()
 
+    def losuj_liczbe(self, x, y):
+        return random.randint(x, y)
 
 
-liczba_
+liczba_losowa = Liczba_losowa()
 
+print(Liczba_losowa.losuj01(liczba_losowa))
 
+print(Liczba_losowa.losuj_liczbe(liczba_losowa, 1, 10))
