652d59a1479b90da5d9fd08eeea8b62808d8babe
kkwolny <kkwolny@vp.pl>
2019-11-05 13:58:54 +0100

    Edit list 3

--- Lista5/zad1.py
+++ Lista5/zad1.py
@@ -7,14 +7,26 @@ class LogExp:
     def __init__(self, a):
         self.a = a
 
+    def logAX(self, x):
+        return math.log(x, self.a)
+
     def log10X(self, x):
         return math.log10(x)
 
-    def logAX(self, x):
-        return math.log(x, self.a)
+    def ax(self, x):
+        return self.a*x
+
+    def logaAX(self, x):
+        return math.log(x, self.ax(x))
 
 
 logExp = LogExp(2)
 
 print(logExp.log10X(10))
 print(logExp.logAX(8))
+print(logExp.ax(10))
+print(logExp.logaAX(10))
+
+
+
+
