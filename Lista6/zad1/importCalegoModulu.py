a64fd1483606d4edf8e9dd486b9dc28e43e08768
kamil <kkwolny@vp.pl>
2019-11-12 00:07:15 +0100

    Add exercise 1, 2

---
+++ Lista6/zad1/importCalegoModulu.py
@@ -0,0 +1,3 @@
+from github.WstepDoProgramowania.Lista6.zad1.foo.function import *
+
+print(pow_sqrt_value())
---
+++ Lista6/zad1/importJawny.py
@@ -0,0 +1,3 @@
+import github.WstepDoProgramowania.Lista6.zad1.foo.function
+
+print(github.WstepDoProgramowania.Lista6.zad1.foo.function.pow_sqrt_value())
