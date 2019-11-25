798c2f9ff0de29aa62f939c32ff02b924a2c6958
kkwolny <kkwolny@vp.pl>
2019-11-25 15:01:33 +0100

    Add working list7

--- Lista7/DiaryCard.py
+++ Lista7/DiaryCard.py
@@ -1,10 +1,14 @@
 class DiaryCard:
-    def __init__(self, title, text):
+    def __init__(self, title, text, date):
         self.title = title
         self.text = text
+        self.date = date
 
     def get_title(self):
         return self.title
 
     def get_text(self):
         return self.text
+
+    def get_date(self):
+        return self.date
