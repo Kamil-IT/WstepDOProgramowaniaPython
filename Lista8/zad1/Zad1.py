f82530714dafadb794c453b93e8da593367119a0
kkwolny <kkwolny@vp.pl>
2019-12-03 16:03:55 +0100

    Fix exercise 1 from list 8

--- Lista8/zad1/Zad1.py
+++ Lista8/zad1/Zad1.py
@@ -4,13 +4,13 @@ import re
 
 def list_pdf_files(include_word, dir_path):
     path_list = []
-    for root, dirs, files in os.walk(dir_path):
+    for root, dirs, files in os.walk(dir_path, topdown=True):
         # Root - ścieżka względna
         # dirs - katalogi w danym podfolderze
         # files - pliki w danym lokacji
         for name in files:
             if name[-4:] == ".pdf" and re.search(include_word, name):
-                path_list.append(os.path.abspath(name))
+                path_list.append(os.path.abspath(root + "\\" + name))
     return path_list
 
 
