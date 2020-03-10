dbeaf5ecfa8ec156192993334c0bc956f77aec53
kkwol <kkwolny@vp.pl>
2020-03-10 16:29:37 +0100

    Fixed bugs in ex1

--- Python(Algorithmics)/list1/ex1.py
+++ Python(Algorithmics)/list1/ex1.py
@@ -42,19 +42,43 @@ def marks_min_max_avg(matrix_indexes):
 
 def stud_with_avg_4_and_more(matrix_indexes):
     list_indexes = matrix_indexes.tolist()
+    list = []
     for index in list_indexes:
         if sum(index) / len(index) >= 4.0:
-            print(index)
+            list.append(index)
+    return list
+
+def stud_max_of_max_marks(matrix_indexes):
+    max_marks = []
+    list_indexes = matrix_indexes.tolist()
+    for mark in np.arange(5.5, 1.5, -0.5):
+        for index in list_indexes:
+            max_marks.append(index.count(mark))
+
+        count_max_mark = max(max_marks)
+        if max_marks.count(count_max_mark) > 1:
+            if mark == 2.0:
+                return list_indexes
+            else:
+                for i in range(len(max_marks) - 1, -1, -1):
+                    if max_marks[i] != count_max_mark and count_max_mark != 0:
+                        list_indexes.pop(i)
+        else:
+            index_max = max_marks.index(count_max_mark)
+            return list_indexes[index_max:index_max + 1]
+        max_marks.clear()
 
 
 def marks_histogerms(matrix):
     histograms = []
     trans_matrix = np.transpose(stud_indexes).tolist()
     for marks in trans_matrix:
-        histograms.append(np.histogram(marks, bins=[2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5]))
+        histograms.append(histograms.append(np.histogram(marks, bins=[2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5])))
+    return histograms
 
 
 stud_indexes = create_random_matrix_marks(3, 3)
+print("Base matrix")
 print(stud_indexes)
 print()
 
@@ -73,31 +97,11 @@ print(max_marks)
 
 # ex3
 print("ex3")
-
-list_indexes = stud_indexes.tolist()
-
-max_marks = []
-for mark in np.arange(5.5, 1.5, -0.5):
-
-    for index in list_indexes:
-        max_marks.append(index.count(mark))
-
-    count_max_mark = max(max_marks)
-    if max_marks.count(count_max_mark) > 1:
-        if mark == 2.0:
-            print(list_indexes)
-        else:
-            for i in range(len(max_marks), 0):
-                if max_marks[i] == count_max_mark:
-                    list_indexes.pop(i)
-    else:
-        index_max = max_marks.index(count_max_mark)
-        print(list_indexes[index_max:index_max + 1])
-    max_marks.clear()
+print(stud_max_of_max_marks(stud_indexes))
 
 # ex4
 print("ex4")
-print(np.histogram(np.transpose(stud_indexes), bins=[2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5]))
+print(marks_histogerms(stud_indexes))
 
 # ex5
 print("ex5")
