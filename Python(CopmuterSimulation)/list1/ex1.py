0f61c081486beb6174836650564decb867ad08f4
kamil-it <kkwolny@vp.pl>
2020-10-12 22:17:57 +0200

    Added ex1 from list 1 from computer simulation, but still missing: średnia logarytmiczna

--- Python(CopmuterSimulation)/list1/ex1.py
+++ Python(CopmuterSimulation)/list1/ex1.py
@@ -1,9 +1,16 @@
 import random
+
 import numpy as np
-from scipy.stats import gmean
-from scipy.stats import hmean
-from scipy.stats import mode
-from scipy.stats import iqr
+from scipy.stats import gmean, hmean, mode, iqr
+
+
+def generalized_mean(array, row):
+    sum = 0.0
+    for number in array:
+        sum += number ^ row
+    sum = sum / len(array)
+    return sum ** (1. / row)
+
 
 randomNumbers = []
 for i in range(0, 1000):
@@ -11,23 +18,23 @@ for i in range(0, 1000):
 arrayNp = np.array(randomNumbers)
 
 # Info about numbers
-print(randomNumbers)
+print("Random 100 numbers: " + randomNumbers.__str__())
+print()
 
 # 1. Avg arithmetic / średnia arytmetyczna
-print("Average arithmetic: " + np.average(arrayNp))
+print("Average arithmetic: " + np.mean(arrayNp).__str__())
 print()
 
 # 2. Avg geometric / średnia geometryczna
-print("Average geometric: " + gmean(randomNumbers))
+print("Average geometric: " + gmean(randomNumbers).__str__())
 print()
 
 # 3. Generalized mean / średnia potęgowa
-# TODO
-print("Generalized mean: ")
+print("Generalized mean: " + generalized_mean(randomNumbers, 3).__str__())
 print()
 
 # 4. Harmean / średnia harmoniczna
-print("Harmean: " + hmean(randomNumbers))
+print("Harmean: " + hmean(randomNumbers).__str__())
 print()
 
 # 5. Logarithmic mean / średnia logarytmiczna
@@ -36,35 +43,29 @@ print("Logarithmic mean: ")
 print()
 
 # 6. Standard deviation / odchylenie standardowe
-print("Standard deviation: " + np.std(arrayNp))
+print("Standard deviation: " + np.std(arrayNp).__str__())
 print()
 
 # 7. Variance / wariancja
-print("Variance: " + np.var(arrayNp))
+print("Variance: " + np.var(arrayNp).__str__())
 print()
 
 # 8. Mode / dominanta
-# TODO: might be error
 print("Mode: " + mode(randomNumbers).__str__())
 print()
 
 # 9. First quartile / pierwszy kwartyl
-# TODO: https://numpy.org/doc/stable/reference/generated/numpy.quantile.html
-print("First quartile: ")
+print("First quartile: " + np.quantile(arrayNp, 0.25).__str__())
 print()
 
 # 10. Second quartile / drugi kwartyl
-# TODO: https://numpy.org/doc/stable/reference/generated/numpy.quantile.html
-print("Second quartile: ")
+print("Second quartile: " + np.quantile(arrayNp, 0.5).__str__())
 print()
 
 # 11. Third quartile / trzeci kwartyl
-# TODO: https://numpy.org/doc/stable/reference/generated/numpy.quantile.html
-print("Third quartile: ")
+print("Third quartile: " + np.quantile(arrayNp, 0.75).__str__())
 print()
 
 # 12. Interquartile range / rozstęp ćwiartkowy
-print("Interquartile range: " + iqr(randomNumbers))
+print("Interquartile range: " + iqr(randomNumbers).__str__())
 print()
-
-
