fa88d6242a405bc47e2405c610fbea77b552c1b1
kamil-it <kkwolny@vp.pl>
2020-10-21 13:05:56 +0200

    Added list in python for internet of things

---
+++ PythonJava(InternetOfThings)/list1/python/ex1_4.py
@@ -0,0 +1,30 @@
+import os
+from datetime import datetime
+
+import pytz
+from flask import Flask, request
+
+app = Flask(__name__)
+path = "/home/kamil/Studies/PythonJava(InternetOfThings)/list1/python/times"
+os.chdir(path)
+
+
+@app.route('/time', methods=['GET'])
+def get_time():
+    tz = request.args.get('tz')
+    return datetime.now(tz=pytz.timezone(tz)).__str__()
+
+
+@app.route('/mqtt', methods=['POST'])
+def post_time():
+    length = 1
+    for root, dirs, names in os.walk(path):
+        length = len(names)
+    time = request.get_data()
+    print(str(time))
+    file = open("time_{}.json".format(length + 1), "w")
+    file.write(str(time))
+    return "success"
+
+
+app.run(port=5002)
