731ecc781c66182252974ba1d9c49dcb3073031f
kamil-it <kkwolny@vp.pl>
2020-11-02 16:21:06 +0100

    Added fake sensor with data

--- PythonJava(InternetOfThings)/list2/fake_sensor.py
+++ PythonJava(InternetOfThings)/list2/fake_sensor.py
@@ -8,27 +8,63 @@ import paho.mqtt.client as mqtt
 import requests
 
 
-# @click.command()
-# @click.option("--config", default='beach_water_quality_config', help='Path with file name to configure app.')
-# def main(config):
-#     return config
+@click.command()
+@click.option("--config_location",
+              default='app5/humidity_config.ini',
+              help='Path with file name to configure app.')
+def main(config_location):
+    print('Configuration location: ' + config_location)
 
-
-def read_config_file_default(config):
     config = configparser.ConfigParser()
-    config.read('beach_water_quality_config.ini')
+    config.read(config_location)
+    print(config.sections())
+
+    protocol_, frequency_, dataSource_ = read_config_file_default(config_location)
+    rows_ = get_data_from_csv_file(get_app_folder(config_location) + "/" + dataSource_)
+    print('Use protocol: ' + protocol_)
+    print('Use frequency: ' + frequency_)
+    print('Use data source: ' + dataSource_)
+    print(f'Processed {len(rows_)} lines.')
+
+    if protocol_ == 'http':
+        url_ = get_http_url(config_location)
+        while True:
+            for row in rows_:
+                send_http_message(url_, json.dumps(row))
+                time.sleep(float(frequency_))
+    elif protocol_ == 'mqtt':
+        print('Use protocol: mqtt')
+        url_, topic_, client_ = get_mqtt_config(config_location)
+        client = mqtt.Client(client_)
+        client.connect(url_)
+        client.subscribe(topic_)
+        while True:
+            for row in rows_:
+                publish_mqtt_message(client, topic_, json.dumps(row))
+                time.sleep(float(frequency_))
+
+
+def read_config_file_default(config_location):
+    config = configparser.ConfigParser()
+    config.read(config_location)
     return config['DEFAULT']['Protocol'], config['DEFAULT']['Frequency'], config['DEFAULT']['Data']
 
 
-def get_http_url():
+def get_http_url(config_location):
     config = configparser.ConfigParser()
-    config.read(get_config_location())
+    config.read(config_location)
     return config['protocol.http']['Url'] + config['protocol.http']['Path']
 
 
-def get_mqtt_config():
+def get_app_folder(config_location):
     config = configparser.ConfigParser()
-    config.read(get_config_location())
+    config.read(config_location)
+    return config['DEFAULT']['App']
+
+
+def get_mqtt_config(config_location):
+    config = configparser.ConfigParser()
+    config.read(config_location)
     return config['protocol.mqtt']['Url'], config['protocol.mqtt']['Topic'], config['protocol.mqtt']['Client']
 
 
@@ -51,36 +87,5 @@ def publish_mqtt_message(client, topic, json_message):
     print("sent: mqtt url:" + topic + " message:" + json_message)
 
 
-config = 'beach_water_quality_config.ini'
-print('Configuration location: ' + config)
-
-config = configparser.ConfigParser()
-config.read('beach_water_quality_config.ini')
-print(config.sections())
-
-
-
-
-protocol_, frequency_, dataSource_ = read_config_file_default(config)
-rows_ = get_data_from_csv_file(dataSource_)
-print('Use protocol: ' + protocol_)
-print('Use frequency: ' + frequency_)
-print('Use data source: ' + dataSource_)
-print(f'Processed {len(rows_)} lines.')
-
-if protocol_ == 'http':
-    url_ = get_http_url()
-    while True:
-        for row in rows_:
-            send_http_message(url_, json.dumps(row))
-            time.sleep(float(frequency_))
-elif protocol_ == 'mqtt':
-    print('Use protocol: mqtt')
-    url_, topic_, client_ = get_mqtt_config()
-    client = mqtt.Client(client_)
-    client.connect(url_)
-    client.subscribe(topic_)
-    while True:
-        for row in rows_:
-            publish_mqtt_message(client, topic_, json.dumps(row))
-            time.sleep(float(frequency_))
+if __name__ == '__main__':
+    main()
