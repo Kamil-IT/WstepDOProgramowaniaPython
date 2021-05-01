04a90f1d3c2c96985e917ad63550a6ce19a199ef
kamil <kkwolny@vp.pl>
2021-05-01 16:35:32 +0200

    Update norms

--- PythonAnaconda(BigData)/1_exam/norms_fit_lo-2.py
+++ PythonAnaconda(BigData)/1_exam/norms_fit_lo-2.py
@@ -1,17 +1,25 @@
 import numpy as np
 
 
-def l2_fit(x0, x, y):
-    return np.sum(np.power(x0[0] * x + x0[1] - y, 2))
-
-
 def l1_fit(x0, x, y):
     return np.sum(np.abs(x0[0] * x + x0[1] - y))
 
 
+def l2_fit(x0, x, y):
+    return np.sum(np.power(x0[0] * x + x0[1] - y, 2))
+
+
 def l0_fit(x0, x, y):
-    sum_ = 0
-    for i in y:
-        if i != 0:
-            sum_ += 1
-    return sum_
+    return np.sum((np.abs(x0[0] * x + x0[1] - y)) > 0)
+
+
+def l0_fit_norm(x0, x, y):
+    return np.linalg.norm((x0[0] * x + x0[1] - y), ord=0)
+
+
+def l1_fit_norm(x0, x, y):
+    return np.linalg.norm((x0[0] * x + x0[1] - y), ord=1)
+
+
+def l2_fit_norm(x0, x, y):
+    return np.linalg.norm((x0[0] * x + x0[1] - y), ord=2)
