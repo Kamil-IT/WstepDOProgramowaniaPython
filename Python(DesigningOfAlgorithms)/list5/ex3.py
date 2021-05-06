73d8aa0f12d8a8123eec4956f8a7bfb666436e97
kamil <kkwolny@vp.pl>
2021-05-06 11:27:27 +0200

    Fixed bubble_sort

--- Python(DesigningOfAlgorithms)/list5/ex3.py
+++ Python(DesigningOfAlgorithms)/list5/ex3.py
@@ -1,19 +1,32 @@
-def quick_sort(array=[12, 4, 5, 6, 7, 3, 1, 15]):
-    """Sort the array by using quicksort."""
+import robot4_0
 
+
+def identify_robot_with_resolution(robots, resolutions):
+    sorted_robots = []
+    for resolution in resolutions:
+        for robot in robots:
+            if robot._resolution == resolution:
+                sorted_robots.append(robot)
+    return sorted_robots
+
+
+def quick_sort(array):
     less = []
     equal = []
     greater = []
 
-    pivot = array[0]
-    for x in array:
-        if x < pivot:
-            less.append(x)
-        elif x == pivot:
-            equal.append(x)
-        elif x > pivot:
-            greater.append(x)
-    return quick_sort(less) + equal + quick_sort(greater)
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        return quick_sort(less) + equal + quick_sort(greater)
+    else:
+        return array
 
 
 def bubble_sort(array):
@@ -29,3 +42,15 @@ def bubble_sort(array):
         if already_sorted:
             break
     return array
+
+
+robots = robot4_0.generate_random_robots(9)
+data = [robot._resolution for robot in robots]
+sort_data_quick = quick_sort(data)
+sort_data_bubble = quick_sort(data)
+
+sorted_robots_quick = identify_robot_with_resolution(robots, sort_data_quick)
+sorted_robots_bubble = identify_robot_with_resolution(robots, sort_data_bubble)
+
+robot4_0.print_all_robots(sorted_robots_quick)
+robot4_0.print_all_robots(sorted_robots_bubble)
